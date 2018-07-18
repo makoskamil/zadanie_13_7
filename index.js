448e9be1bd92637304035c49c60467c9030ae043
Kamil Makoś <makos.kamil@o2.pl>
2018-07-18 13:34:16 +0200

    add_files

---
+++ index.js
@@ -0,0 +1,21 @@
+var fs = require('fs');
+
+fs.readdir('./.git', 'utf-8', function(err, files){
+	if(err) throw err;
+	var directoryContent = files;
+		
+	for(var i=0; i<directoryContent.length; i++){
+		var position = '\n' + directoryContent[i];
+		console.log(position);
+		fs.appendFile('./gitdirectory.txt', position , function(err){
+			if(err) throw err;
+			});
+	}
+	console.log('\nWriting the file done');
+});
+
+
+fs.writeFile('./gitdirectory.txt', '', function(err, data){
+	if(err) throw err;
+	console.log('\nClearing the file done');
+});
